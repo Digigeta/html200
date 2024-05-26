f627fb3fd91d80e702597041cdfb73d67ad4ee48
Digigeta <mikaelmulugeta44@gmail.com>
2024-05-26 01:04:45 -0700

    Add countdown.js for Lesson02 challenge

---
+++ lesson02/countdown.js
@@ -0,0 +1,8 @@
+// Initialize the countdown number
+let number = 10;
+
+// Use a loop to countdown from the number to 0
+while (number >= 0) {
+  console.log(number);
+  number--; // Decrease the number by 1
+}
