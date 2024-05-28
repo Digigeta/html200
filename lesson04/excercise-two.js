143c0c45226ef872608d46807dfa8b3913e5f2ef
Digigeta <mikaelmulugeta44@gmail.com>
2024-05-27 21:51:08 -0700

    added the five words file

---
+++ lesson04/excercise-two.js
@@ -0,0 +1,8 @@
+// Take the sentence "UW has a breathtaking campus."
+let sentence = "UW has a breathtaking campus.";
+
+// Make an array from that sentence
+let wordsArray = sentence.split(' ');
+
+// Console.log the third word
+console.log('The third word is:', wordsArray[2]);
