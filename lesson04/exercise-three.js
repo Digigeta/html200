af6d37c6b56d9c3a2b23c34354f5afd538a84cb7
Digigeta <mikaelmulugeta44@gmail.com>
2024-05-27 22:07:20 -0700

    Adding a Loop

---
+++ lesson04/exercise-three.js
@@ -0,0 +1,7 @@
+// Create an array with at least 5 strings
+let animals = ['elephants', 'tigers', 'lions', 'giraffes', 'zebras'];
+
+// Use a for loop to console log all of the elements plus the index and a comma
+for (let i = 0; i < animals.length; i++) {
+  console.log((i + 1) + ',' + animals[i]);
+}
